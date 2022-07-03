4843853fc137c5c235b6006f75bf57a1dd9d769b
Francis Ong <francis.ong25@gmail.com>
2022-07-03 13:27:57 +0800

    initial commit, add userdata model

---
+++ src/main/java/com/mycompany/java/essentials/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.java.essentials;
+
+// Imports
+import com.mycompany.java.essentials.model.UserData;
+
+/**
+ *
+ * @author franc
+ */
+public class Main {
+    public static void main (String[]args) {
+        UserData userData_1 = new UserData("francis", "54321", "admin");
+        UserData userData_2 = new UserData("guest", "12345", "customer");
+
+    }
+}
---
+++ src/main/java/com/mycompany/java/essentials/model/UserData.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.java.essentials.model;
+
+/**
+ *
+ * @author franc
+ */
+public class UserData {
+    
+    private String userName;
+    private String passWord;
+    private String userType;
+    
+    public UserData(String userName, String passWord, String userType) {
+        this.userName = userName;
+        this.passWord = passWord;
+        this.userType = userType;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+    
+}
